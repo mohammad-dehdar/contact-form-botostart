d6ea0e0472825e1492278ed96fe1db2923364eed
mohammad dehdar <dehdardevelop@gmail.com>
2024-08-18 19:32:42 +0330

    add interface

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ['./*.{html,js}'],
   theme: {
     extend: {
+      colors:{
+        cyan: colors.cyan,
+        teal: colors.teal
+      },
       fontFamily: {
         karla: ['Karla', 'sans-serif'], 
       }
